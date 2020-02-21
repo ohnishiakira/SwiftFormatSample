4146fd9a0defe3f6d3526f9f41bf0c404eafc67a
Akira Ohnishi <s06206ao@gmail.com>
2020-02-21 15:08:43 +0900

    Initial Commit

---
+++ SwiftFormatSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFormatSample
+//
+//  Created by Akira Ohnishi on 2020/02/21.
+//  Copyright © 2020 Akira Ohnishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
